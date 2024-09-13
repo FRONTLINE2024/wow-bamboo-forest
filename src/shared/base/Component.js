b7e0d357a918faebc5a4139ce3d675d8e77f419b
hamo-o <yheel369@gmail.com>
2024-09-14 04:26:59 +0900

    chore: eslint 관련 의존성 변경 및 클래스 jsdoc 추가

--- src/shared/base/Component.js
+++ src/shared/base/Component.js
@@ -1,23 +1,42 @@
+/**
+ * @description 공용 컴포넌트 클래스
+ */
 export class Component {
-  #target; // 컴포넌트를 넣을 부모
+  #target;
 
   #props;
 
+  /**
+   *
+   * @param {HTMLElement} target - 컴포넌트를 렌더링할 부모 요소
+   * @param {any} props - 컴포넌트에 전달할 데이터
+   */
   constructor(target, props) {
     this.#target = target;
     this.#props = props;
   }
 
-  static html(strings, ...values) {
-    return String.raw(strings, ...values);
+  /**
+   *
+   * @param {string} htmlString - HTML 문자열 리터럴
+   * @returns {string} HTML 문자열을 반환합니다.
+   */
+  static html(htmlString) {
+    return String.raw(htmlString);
   }
 
+  /**
+   * @returns {string} UI 템플릿을 반환합니다.
+   */
   static template() {
     // UI 구성
     return "";
   }
 
+  /**
+   *  @description 컴포넌트를 `target`의 자식으로 렌더링합니다.
+   */
   render() {
-    this.#target.innerHTML = this.constructor.template(); // UI 렌더링
+    this.#target.innerHTML = this.constructor.template();
   }
 }
