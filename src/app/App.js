e307f6390012b7df58fac652afd59556b750b57f
hamo-o <yheel369@gmail.com>
2024-09-26 01:20:52 +0900

    feat: App 자식으로 Card 컴포넌트 렌더링

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,21 @@
 import { Component } from "../shared";
-
+import { Card } from "../widgets";
 export default class App extends Component {
   setup() {
+    this.element = document.createElement("main");
+    this.element.id = "my-app";
     this.state = { text: "Hello, World!" };
   }
 
   template() {
     const { text } = this.state;
-    return /* html */ `<main data-component="my-app">${text} 를 써봅니다</main>`;
+    this.element.innerHTML = `${text} 를 써봅니다`;
+
+    return this.element;
+  }
+
+  setTemplate() {
+    const card = new Card(this.element);
+    card.render();
   }
 }
