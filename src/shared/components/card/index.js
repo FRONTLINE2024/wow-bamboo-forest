ef8e663456eefd877705809c030cacfc96733837
hamo-o <yheel369@gmail.com>
2024-09-26 01:28:21 +0900

    rename: Card 컴포넌트 shared 폴더로 이동

--- src/shared/components/card/index.js
+++ src/shared/components/card/index.js
@@ -1,14 +1,24 @@
-import { Component, fonts } from "../../shared";
+import { Component } from "../../base";
+import { fonts } from "../../styles";
 
 import styles from "./card.module.scss";
 
+/**
+ * @description 카드 컴포넌트
+ */
 export class Card extends Component {
+  /**
+   *
+   */
   setup() {
     this.element = document.createElement("div");
     this.element.className = styles.card;
     this.state = { content: "Hello, World!" };
   }
 
+  /**
+   * @returns {HTMLElement} - 카드 컴포넌트를 반환합니다.
+   */
   template() {
     const num = this.target.children.length + 1;
     const { content } = this.state;
--- src/shared/index.js
+++ src/shared/index.js
@@ -1,2 +1,3 @@
 export * from "./base";
 export * from "./styles";
+export * from "./components";
