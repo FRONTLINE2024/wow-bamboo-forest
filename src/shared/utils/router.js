cba710273ce764263a09de4598ce76f0c54d89b0
hamo-o <yheel369@gmail.com>
2024-09-27 14:37:00 +0900

    refactor: router 관심사별 분리 및 폴더 위치이동

---
+++ src/shared/utils/router.js
@@ -0,0 +1,65 @@
+let routes = {};
+
+/**
+ *
+ * @param {string} url - URL
+ * @returns {object} - pathname, params
+ */
+export const usePathName = (url) => {
+  const pathname = url.split("?")[0];
+  const params = Object.fromEntries(new URLSearchParams(url.split("?")[1]));
+
+  return { pathname, params };
+};
+
+/**
+ * @returns {object} - 라우터 객체
+ */
+export const useRouter = () => {
+  window.addEventListener("popstate", () => {
+    if (routes[location.pathname]) {
+      routes[location.pathname]();
+    }
+  });
+
+  /**
+   *
+   * @param {string} url - 이동할 URL
+   * @param {boolean} isInit - 초기화 여부
+   */
+  const move = (url, isInit = false) => {
+    const { pathname, params } = usePathName(url);
+
+    if (routes[pathname]) {
+      if (!isInit) history.pushState({}, "", url);
+
+      routes[pathname]({
+        searchParams: params,
+      });
+      return;
+    }
+
+    location.href = url;
+  };
+
+  /**
+   * @param {string} url - 이동할 URL
+   */
+  const push = (url) => {
+    move(url);
+  };
+
+  /**
+   *
+   * @param {object} param - 초기화할 라우터 정보
+   */
+  const init = (param) => {
+    routes = param.routes;
+    move(location.pathname + location.search, true);
+  };
+
+  return {
+    init,
+    push,
+  };
+};
