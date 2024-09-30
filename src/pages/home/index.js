86a8b8b46a1c35801636c7aea632676123a4dd57
hamo-o <yheel369@gmail.com>
2024-10-01 02:10:01 +0900

    feat: 쿼리파라미터 전달하기

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,13 +2,15 @@ import { useRouter } from "../../shared";
 
 /**
  *
+ * @param {object} params - Home에 전달할 데이터
+ * @param {object} params.searchParams - 쿼리파라미터 객체
  */
-export const Home = () => {
+export const Home = ({ searchParams }) => {
   const router = useRouter();
 
   document.getElementById("#app").innerHTML = `
   <nav>
-    <div>Home</div>
+    <div>Home ${searchParams?.test || ""}</div>
     <ul>
       <li><button class="home">Home</button></li>
       <li><button class="mypage">Mypage</button></li>
@@ -22,6 +24,6 @@ export const Home = () => {
   });
 
   document.querySelector(".mypage").addEventListener("click", () => {
-    router.push("/mypage");
+    router.push("/mypage?test=123");
   });
 };
