c7c0cac01b8113034526219ef5931218fcad5a3f
hamo-o <yheel369@gmail.com>
2024-10-05 04:04:56 +0900

    feat: Home 페이지 변경

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,29 +1,29 @@
-import { useRouter } from "../../shared";
+import { createPost } from "../../features";
+import { Component, Card } from "../../shared";
+export class Home extends Component {
+  setup() {
+    this.element.id = "my-app";
+    this.state = { text: "Hello, World!" };
+  }
 
-/**
- *
- * @param {object} params - Home에 전달할 데이터
- * @param {object} params.searchParams - 쿼리파라미터 객체
- */
-export const Home = ({ searchParams }) => {
-  const router = useRouter();
+  template() {
+    const { text } = this.state;
+    return `${text} 를 써봅니다`;
+  }
 
-  document.getElementById("#app").innerHTML = `
-  <nav>
-    <div>Home ${searchParams?.test || ""}</div>
-    <ul>
-      <li><button class="home">Home</button></li>
-      <li><button class="mypage">Mypage</button></li>
-      <li><a is="my-link" href="/mypage">커스텀 링크</a></li>
-    </ul>
-  </nav>
-`;
+  mounted() {
+    const card = new Card(this.element, {
+      num: this.element.children.length + 1,
+    });
 
-  document.querySelector(".home").addEventListener("click", () => {
-    router.push("/");
-  });
+    card.element.addEventListener("click", (event) => {
+      event.stopPropagation();
 
-  document.querySelector(".mypage").addEventListener("click", () => {
-    router.push("/mypage?test=123");
-  });
-};
+      createPost();
+      card.setState({
+        number: card.state.number + 1,
+        content: `${card.state.number} 번째 클릭`,
+      });
+    });
+  }
+}
